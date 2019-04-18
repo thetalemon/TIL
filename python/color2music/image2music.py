517c846d333415a55d2b973ed68ede47567638f1
thetalemon <sasami0314@gmail.com>
2019-04-18 22:25:38 +0900

    refactoring code

--- python/color2music/image2music.py
+++ python/color2music/image2music.py
@@ -10,7 +10,7 @@ def calcRGB(img_array):
     return average_color
 
 def makeNote(note, startTime, noteLong, velocity):
-    endTime =  startTime + noteLong
+    endTime = startTime + noteLong
     note = pretty_midi.Note(velocity=velocity, pitch=note, start=startTime, end=endTime)
     return note
 
@@ -39,41 +39,46 @@ def main():
         noteList = makeAllChordList(0);
 
         noteTime = 0
-        updownFlag = np.random.randint(0,2)
         termLong = 4
         noteLong = 1
         noteNumber = 0
-        for a in range(0,12):
-            baseNote = np.random.randint(0,7)
-            instrument.notes.append(makeNote(noteList[7+baseNote], noteTime, termLong, 75))
-            instrument.notes.append(makeNote(noteList[7+baseNote+2], noteTime, termLong, 75))
-            instrument.notes.append(makeNote(noteList[7+baseNote+4], noteTime, termLong, 75))
-
+        updownFlag = np.random.randint(0,2)
+        for terms in range(0,12):
+            baseNote = 7+np.random.randint(0,7)
+            ## set base notes
+            for chords in range(0,7,2):
+                instrument.notes.append(makeNote(noteList[baseNote+chords], noteTime, termLong, 75))
             randomNumber = np.random.randint(0,5)*2 - 4
             firstNoteNumber = 7*4 + baseNote + randomNumber
 
-            if noteNumber!=0:
-                while (noteNumber-firstNoteNumber) > 6:
-                    randomNumber = np.random.randint(0,10)*2 - 4
+            ## 最初の小節でないなら
+            if terms != 0:
+                ## 前小節の最後の音と、今小節の最初の音の差を、6以下に制限する
+                while (noteNumber - firstNoteNumber) > 6:
+                    ## 音の範囲が狭いと、6以下にできないときがあるため、ランダムの範囲を広げる
+                    randomNumber = np.random.randint(0,7)*2 - 4
                     firstNoteNumber = 7*4 + baseNote + randomNumber
 
             instrument.notes.append(makeNote(noteList[firstNoteNumber], noteTime, noteLong, 100))
-
             continusCount = 0
             for a in range(1,4):
-                if noteNumber > 35 : noteNumber = noteNumber  - 10
+                ## 前の音が低い・高い音なら、音を上げ・下げておく
+                if   noteNumber > 35 : noteNumber = noteNumber  - 10
                 elif noteNumber < 23 : noteNumber = noteNumber + 10
 
                 randomNumber = np.random.randint(0,4)*2
-                if continusCount<1 : continusCount = continusCount+1
-                else :
-                    randomNumber = np.random.randint(1,4)*2
 
-                if updownFlag==1 : randomNumber = randomNumber * -1
+                ## 前の音と同じなら連続カウントを１上げる。
+                ## 連続カウントが１以上なら、変化なし以外で再度差分を求める。
+                if randomNumber == 0 and continusCount < 1 : continusCount = continusCount+1
+                else : randomNumber = np.random.randint(1,4)*2
+
+                ## 上げ下げフラグが１なら、差分値の正負を反転
+                if updownFlag == 1 : randomNumber = randomNumber * -1
                 noteNumber = firstNoteNumber + randomNumber
 
                 # print("listsize:",len(noteList))
-                print(noteNumber)
+                print(randomNumber,",",noteNumber)
                 instrument.notes.append(makeNote(noteList[noteNumber], noteTime + a, noteLong, 100))
                 pm.instruments.append(instrument)
 
