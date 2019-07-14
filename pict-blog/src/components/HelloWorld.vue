79c59431748e2a3311798c0ef4af135033a6162d
thetalemon <sasami0314@gmail.com>
2019-07-14 21:00:48 +0900

    mod indent

--- pict-blog/src/components/HelloWorld.vue
+++ pict-blog/src/components/HelloWorld.vue
@@ -2,33 +2,17 @@
   <div class="hello">
     <h1>Hello {{ name }}!!</h1>
     <h1>{{ msg }}</h1>
-    <!-- <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul> -->
+
     <button @click="signOut">Sign out</button>
       <table>
         <tr>発言一覧</tr>
-        <!-- <tr>{{comments}}<tr> -->
         <tr v-for="(comment, index) in comments" :key="index">
-        <td><img :src="comment.avatar"></td>
+          <td><img :src="comment.avatar"></td>
           <td>{{comment.content}}</td>
-            <td>{{comment.createdAt}}</td>
-        <td>{{comment.id}}</td>
-      </tr>
-    </table>
+          <td>{{comment.createdAt}}</td>
+          <td>{{comment.id}}</td>
+        </tr>
+      </table>
   </div>
 </template>
 
@@ -67,12 +51,6 @@ export default {
         console.log('Error getting documents', err)
       })
   }
-  // firestore () {
-  //   return {
-  //     // firestoreのcommentsコレクションを参照
-  //     comments: firebase.firestore().collection('comments').orderBy('createdAt')
-  //   }
-  // }
 }
 </script>
 
