806937b1642231a5dc49c8789c77b7b75bc64150
thetalemon <sasami0314@gmail.com>
2019-07-15 13:45:05 +0900

    add modal window

--- pict-blog/src/components/HelloWorld.vue
+++ pict-blog/src/components/HelloWorld.vue
@@ -4,15 +4,19 @@
     <h1>{{ msg }}</h1>
 
     <button @click="signOut">Sign out</button>
-      <table>
-        <tr>発言一覧</tr>
-        <tr v-for="(comment, index) in comments" :key="index">
-          <td><img :src="comment.avatar"></td>
-          <td>{{comment.content}}</td>
-          <td>{{comment.createdAt}}</td>
-          <td>{{comment.id}}</td>
-        </tr>
-      </table>
+    <table align="center">
+      <tr>発言一覧</tr>
+      <tr v-for="(comment, index) in comments" :key="index">
+        <td><img class="top" :src="comment.avatar" v-on:click="show"></td>
+        <modal name="img-modal" :adaptive="true"  width="80%" height="80%">
+          <p><img :src="comment.avatar" v-on:click="show"></p>
+          <p>{{comment.content}}</p>
+          <p>{{comment.createdAt}}</p>
+          <p><button v-on:click="hide">hide</button></p>
+        </modal>
+      </tr>
+    </table>
+      <!-- <button v-on:click="show">show!</button> -->
   </div>
 </template>
 
@@ -20,9 +24,16 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+// import VModal from 'vue-js-modal'
+// import VModal from 'vue-js-modal'
+// Vue.use(VModal)
 
 export default {
+  component: ('modal', {
+    template: '#modal-template'
+  }),
   name: 'HelloWorld',
+  // el: '.l-wrapper',
   data: () => ({
     msg: 'Welcome to Your Vue.js App',
     name: firebase.auth().currentUser.email,
@@ -33,6 +44,12 @@ export default {
       firebase.auth().signOut().then(() => {
         this.$router.push('/signin')
       })
+    },
+    show: function () {
+      this.$modal.show('img-modal')
+    },
+    hide: function () {
+      this.$modal.hide('img-modal')
     }
   },
   created: function () {
@@ -59,15 +76,45 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-ul {
+/* ul {
   list-style-type: none;
   padding: 0;
 }
 li {
   display: inline-block;
   margin: 0 10px;
-}
-a {
+} */
+/* a {
   color: #42b983;
+} */
+.hello{
+ text-align: center;
 }
+table{
+  border: 2px #2b2b2b solid;
+}
+td{
+ text-align: center;
+}
+img {
+  align:center;
+  padding: 30px;
+  margin: 20px;
+}
+div {
+  text-align:center;
+}
+modal{
+  text-align:center;
+}
+img.top {
+  width: 250px;
+  height: 250px;
+  object-fit: cover; /* この一行を追加するだけ！ */
+}
+
+p{
+  text-align: center;
+}
+
 </style>
--- pict-blog/src/main.js
+++ pict-blog/src/main.js
@@ -4,11 +4,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import firebase from 'firebase/app'
+import VModal from 'vue-js-modal'
 
 Vue.config.productionTip = false
+Vue.use(VModal)
 
 const config = require('./config.js')
 firebase.initializeApp(config)
+
 
 /* eslint-disable no-new */
 new Vue({
