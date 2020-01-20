b321bd769a1a7bf486c8d569864c4ff9c37f473d
thetalemon <sasami0314@gmail.com>
2020-01-20 23:46:07 +0900

    change eye-catch to photo

--- manas-portfolio/src/components/pages/EyeCatch.vue
+++ manas-portfolio/src/components/pages/EyeCatch.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="eye-catch">
-    <img src="../../assets/IMG_0235.jpg">
-    <div>M's portfolio</div>
+    <div id="eye-catch-image">M's portfolio</div>
+    <img src="../../assets/IMG_8003.jpg">
   </div>
 </template>
 
@@ -17,11 +17,23 @@ export default {
 <style lang="scss" scoped>
 @import '../../unified.scss';
 #eye-catch {
+  position: relative;;/*絶対配置*/
   background-color: $dark-background-color;
-  padding: $wide $zero;
   margin: $little $little;
+  padding: $micro;
+  height: 300px;
+  #eye-catch-image {
+    position: absolute;
+    color: $dark-background-color;
+    top: 150px;
+    left: 50px;
+    font-size: 2.0em;
+  }
   img{
-    width: 200px;
+    width: 100%;
+    height: 300px;
+    object-position: 100% 50%;
+    object-fit: cover;
   }
 }
 </style>
