f17688ed5b5d5a3de76958acb6bb7dcb6b450f92
thetalemon <sasami0314@gmail.com>
2020-01-28 23:11:08 +0900

    start create ywt-service

---
+++ ywt-service/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    過去データから続きを実行みたいな感じでここにテキストボックスをおく<br>
+    convertボタン<br>
+    なんか入力欄<br>
+    項目名<br>
+    よていは<br>
+    やった<br>
+    わかった<br>
+    つぎは<br>
+    outputボタン<br>
+    ここにアウトプットが出力されるテキストボックスをおく<br>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
