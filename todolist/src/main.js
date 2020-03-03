96eb5faffcc99a76001835a4423cf076ca1dccce
thetalemon <sasami0314@gmail.com>
2020-03-03 15:38:51 +0900

    add firebase setup and test

--- todolist/src/main.js
+++ todolist/src/main.js
@@ -2,12 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Firebase from './firebase'
+import store from './store'
 
 Vue.config.productionTip = false
 
+Firebase.init()
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
