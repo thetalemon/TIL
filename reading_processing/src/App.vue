99049fdfa0ddfbd21377dafc68e3386f3c7a6a2d
thetalemon <sasami0314@gmail.com>
2020-05-24 21:36:33 +0900

    impl reading-processing

---
+++ reading_processing/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  overflow: scroll;
+}
+</style>
