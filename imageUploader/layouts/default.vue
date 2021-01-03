29291a00cfe62464d4663118a2c4919c4a796d0d
thetalemon <sasami0314@gmail.com>
2021-01-03 22:19:35 +0900

    impl image uploader frontend

---
+++ imageUploader/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="abs" app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '画像アップローダー',
+      abs: true,
+    }
+  },
+}
+</script>
---
+++ imageUploader/pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <upload-card
+          v-if="!uploaded"
+          :uploading="uploading"
+          @click-upload="onClickUpload($event)"
+        />
+        <upload-end v-if="uploaded" />
+        <upload-dialog :active="uploading" @upload-end="onUploadEnd()" />
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import UploadCard from '~/components/UploadCard.vue'
+import UploadDialog from '~/components/UploadDialog.vue'
+import UploadEnd from '~/components/UploadEnd.vue'
+
+export default {
+  components: {
+    UploadCard,
+    UploadDialog,
+    UploadEnd,
+  },
+  data() {
+    return {
+      uploading: false,
+      uploaded: false,
+    }
+  },
+  methods: {
+    onClickUpload() {
+      this.uploading = true
+    },
+    onUploadEnd() {
+      this.uploading = false
+      this.uploaded = true
+    },
+  },
+}
+</script>
